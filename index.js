d36f8a064fd0650a73be7201a411f77fe4597c35
Fabian Fabro <fabrofabian@gmail.com>
2019-03-12 04:29:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ function takeANumber(currentLine, name){
   currentLine.push(name);
   return `Welcome, ${name}. You are number ${currentLine.length} in line.`;
 }
+
+function nowServing(katzDeliLine){
+  if (katzDeliLine.length === 0){
+    return "There is nobody waiting to be served!"
+  }
+  else{
+    return katzDeliLine.shift();
+  }
+}
